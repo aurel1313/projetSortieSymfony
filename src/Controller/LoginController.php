d5f9e088b5aad58e63936cb4fd32bb77cb1552e4
Aurelien FABRE <aurelien.fabre2022@campus-eni.fr>
2023-10-20 09:22:45 +0200

    change error message connexion

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => "Mauvais mot de passe ou Email"]);
     }
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginAuthenticator $loginAuthenticator, EntityManagerInterface $entityManager): Response
