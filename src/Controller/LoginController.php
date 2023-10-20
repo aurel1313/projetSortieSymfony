1ca3fb519b9daa549b4d6e364b87825e733d29d8
Corentin CADU <corentin.cadu2022@campus-eni.fr>
2023-10-20 09:52:58 +0200

    connexion ajout titre

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -20,17 +20,12 @@ class LoginController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => "Mauvais mot de passe ou Email"]);
     }
+
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginAuthenticator $loginAuthenticator, EntityManagerInterface $entityManager): Response
     {
